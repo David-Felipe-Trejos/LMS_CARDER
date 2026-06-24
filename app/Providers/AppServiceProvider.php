4428d10a66da554f4b05962264c786453b132b43
David Felipe Trejos Miranda <dftrejos@carder.gov.co>
2026-06-24 15:46:01 -0500

    fix: fecha de certificado en español

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Carbon\Carbon;
 class AppServiceProvider extends ServiceProvider {
     public function register(): void {}
-    public function boot(): void { Paginator::useBootstrapFive(); }
+    public function boot(): void { 
+        Paginator::useBootstrapFive();
+        Carbon::setLocale('es');
+    }
 }
